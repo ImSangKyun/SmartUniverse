d958e91f757609254aca0c63f59cc2ae4a0682ac
dongseoki <start0is5half@gmail.com>
2020-02-12 06:11:45 -0100

    hi...

--- RPI3/boot/Main.c
+++ RPI3/boot/Main.c
@@ -1,9 +1,9 @@
 #include "HalUart.h"
 #include "stdint.h"
-#include "stdio.h"
+//#include "stdio.h"
 
 static void Hw_init(void);
-static void Printf_test(void);
+// static void Printf_test(void);
 
 void main(void)
 {
@@ -32,18 +32,18 @@ static void Hw_init(void)
     Hal_uart_init();
 } 
 
-static void Printf_test(void)
-{
-    char* str = "printf pointer test";
-    char* nullptr = 0;
-    uint32_t i = 5;
-    uint32_t* sysctrl0 = (uint32_t*)0x10001000;
+// static void Printf_test(void)
+// {
+//     char* str = "printf pointer test";
+//     char* nullptr = 0;
+//     uint32_t i = 5;
+//     uint32_t* sysctrl0 = (uint32_t*)0x10001000;
 
-    debug_printf("%s\n", "Hello printf");
-    debug_printf("output string pointer: %s\n", str);
-    debug_printf("%s is null pointer, %u number\n", nullptr, 10);
-    debug_printf("%u = 5\n", i);
-    debug_printf("dec=%u hex=%x\n", 0xff, 0xff);
-    debug_printf("print zero %u\n", 0);
-    debug_printf("SYSCTRL0 %x\n", *sysctrl0);
-}
+//     debug_printf("%s\n", "Hello printf");
+//     debug_printf("output string pointer: %s\n", str);
+//     debug_printf("%s is null pointer, %u number\n", nullptr, 10);
+//     debug_printf("%u = 5\n", i);
+//     debug_printf("dec=%u hex=%x\n", 0xff, 0xff);
+//     debug_printf("print zero %u\n", 0);
+//     debug_printf("SYSCTRL0 %x\n", *sysctrl0);
+// }
