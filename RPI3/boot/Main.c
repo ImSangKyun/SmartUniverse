f2aa68e0f4f339bd180f1d7f73bd3f74db694858
dongseoki <start0is5half@gmail.com>
2020-02-12 05:46:33 -0100

    printf_test

--- RPI3/boot/Main.c
+++ RPI3/boot/Main.c
@@ -1,6 +1,9 @@
 #include "HalUart.h"
+#include "stdint.h"
+#include "stdio.h"
 
 static void Hw_init(void);
+static void Printf_test(void);
 
 void main(void)
 {
@@ -9,6 +12,10 @@ void main(void)
      Hal_uart_put_char('U');
     // Hal_uart_put_char('\r');
      Hal_uart_put_char('\n');
+
+    putstr("Hello World!\n");
+
+    Printf_test();
 
     while(1)
     {
@@ -24,3 +31,19 @@ static void Hw_init(void)
 {
     Hal_uart_init();
 } 
+
+static void Printf_test(void)
+{
+    char* str = "printf pointer test";
+    char* nullptr = 0;
+    uint32_t i = 5;
+    uint32_t* sysctrl0 = (uint32_t*)0x10001000;
+
+    debug_printf("%s\n", "Hello printf");
+    debug_printf("output string pointer: %s\n", str);
+    debug_printf("%s is null pointer, %u number\n", nullptr, 10);
+    debug_printf("%u = 5\n", i);
+    debug_printf("dec=%u hex=%x\n", 0xff, 0xff);
+    debug_printf("print zero %u\n", 0);
+    debug_printf("SYSCTRL0 %x\n", *sysctrl0);
+}
