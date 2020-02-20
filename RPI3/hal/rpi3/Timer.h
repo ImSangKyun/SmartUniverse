4088d61b90dab8fd0f899ba85218a777041dc7e3
dongseoki <start0is5half@gmail.com>
2020-02-20 08:39:36 -0100

    Timer.h is being repaired

--- RPI3/hal/rpi3/Timer.h
+++ RPI3/hal/rpi3/Timer.h
@@ -1,5 +1,5 @@
-#ifndef HAL_RPI3_DELAY_H_
-#define HAL_RPI3_DELAY_H_
+#ifndef HAL_RPI3_TIMER_H_
+#define HAL_RPI3_TIMER_H_
 
 typedef union CS_t
 {
@@ -12,6 +12,58 @@ typedef union CS_t
 	}bits;
 } CS_t;
 
+
+// from now, timer register structs begin...
+typedef union CONTROL_t
+{
+	uint32_t all;
+	struct {
+		uint32_t RESERVED0:1; // 0
+		uint32_t BC:1; //1 Bit Counter.. 16 or 23.
+		uint32_t PSB:2; //3:2 Pre-scale bits
+		uint32_t RESERVED1:1; //4
+		uint32_t TIE:1; //5 timer interrupt enable..
+		uint32_t MODE:1; //6. not used 1 periodic mode will not be supported...
+		uint32_t TE:1; //7 timer enabled..
+		uint32_t DHMTH:1; //8 Timers halted if ARM is in debug halted mode
+		uint32_t FRCE:1; //9 free running counter enabled.
+		uint32_t RESERVED2:6; // 15:10 
+		uint32_t FRCPS:8; //23:16
+	}bits;
+} CONTROL_t;
+
+
+
+typedef union RIRQ_t
+{
+	uint32_t all;
+	struct {
+		uint32_t IPB:1; // 0
+	}bits;
+} RIRQ_t;
+
+typedef union MIRQ_t
+{
+	uint32_t all;
+	struct {
+		uint32_t ILIS:1; //0
+	}bits;
+} MIRQ_t;
+
+typedef union PD_t
+{
+	uint32_t all;
+	struct {
+		uint32_t PDV:10; // 9:0
+	}bits;
+} PD_t;
+
+
+// timer register end..
+
+
+
+
 typedef struct S_Timer_t
 {
 	CS_t cs; //	0x0 CS System Timer Control/Status 32
@@ -23,4 +75,19 @@ typedef struct S_Timer_t
 	uint32_t c3;//0x18 C3 System Timer Compare 3 32
 }S_Timer_t;
 
-#endif /* HAL_RPI3_DELAY_H_ */
+
+typedef struct Timer_t
+{
+	uint32_t load;		// 0x400 Load
+	uint32_t value;		// 0x404 Value (Read Only)
+	CONTROL_t control;	// 0x408 Control
+	uint32_t irqca; 		// 0x40C IRQ Clear/Ack (Write only)
+	RIRQ_t rirq; 		// 0x410 RAW IRQ (Read Only)
+	MIRQ_t mirq;		// 0x414 Masked IRQ (Read Only)
+	uint32_t reload;	// 0x418 Reload
+	PD_t pd;			// 0x41C Pre-divider (Not in real 804!)
+	FRC_t frc;			// 0x420 Free running counter (Not in real 804!)
+}Timer_t;
+
+
+#endif /* HAL_RPI3_TIMER_H_ */
